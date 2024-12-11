0ea6ea3702c33dba8a6bdfdd015eccf4598b0651
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-12-11 20:02:11 +0300

    Getting Input

---
+++ Getting Input.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+print('Hi ' + name)
+name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + favorite_color)
