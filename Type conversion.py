8ff04e087378510a8b6f504da0fa1ed16464a619
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-12-11 20:14:23 +0300

    Type conversion

---
+++ Type conversion.py
@@ -0,0 +1,9 @@
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_in_pounds = input('what is your weight in pounds? ')
+weight_in_kg = int(weight_in_pounds) * 0.45
+print(weight_in_kg)
