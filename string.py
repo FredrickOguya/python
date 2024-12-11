5bd2fdc7809ca9cfc6b4fcee22ea18bbf702b449
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-12-11 20:14:44 +0300

    string

---
+++ string.py
@@ -0,0 +1,18 @@
+course = 'Python for "Beginners"'
+course = "Python's for beginners"
+course = '''
+Hi Fred,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+'''
+course = 'Python for Beginners'
+another = course[:]
+
+
+print(another)
+
+name = 'Jennifer'
+print(name[1:-1])
