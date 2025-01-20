9516ebdb7b0ea67dd63b2ac9b24084bbc396ecd2
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 13:49:30 +0300

    exercise

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,15 +1,2 @@
-course = '''
-Hi John
-
-Here is our first email
-
-Thanks for
-your support
-
-thanks
-'''
-print(course)
-course = 'Python for Begginers'
-print(course[0:5])
-another = course[:3]
-print(another)
+name = 'Jennifer'
+print(name[1:-1])
