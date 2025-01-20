a9abb3fec4bde629304843a7638531bb9bc88e97
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 12:43:23 +0300

    exercise

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,5 +1,3 @@
-birth_year = input("Birth year: ")
-print(type(birth_year))
-age = 2024 - int(birth_year)
-print(type(age))
-print(age)
+Weight_in_pounds = input("What is your weight in pounds? ")
+weight_in_kilograms= int(Weight_in_pounds) * 0.45
+print(weight_in_kilograms )
