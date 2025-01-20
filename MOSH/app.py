11172715e6a8be1e0d582762fd728d2c5c3a4550
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 15:01:22 +0300

    exercise

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,7 +1,7 @@
 name = input("Enter Your Name")
 if len(name)< 3:
     print("Name must be at least 3 characters")
-elif len(name>50):
+elif len(name)>50:
     print("name can be a maximum of 50 characters")
 else:
     print("Name looks good!")
