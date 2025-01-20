3bb6597d7618d25a83b9e2a8d6ffe4cb9c2d4a6f
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 12:12:36 +0300

    exercise

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,2 +1,3 @@
-name = input('What is your name? ')
-print('Hi ' + name)
+name = input('What is Your name?')
+favourite_color = input('What is your favourite color? ')
+print(name +' likes ' + favourite_color)
