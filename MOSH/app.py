91f05b1c4fadca99a4c701474d794f210ef239a2
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 14:01:12 +0300

    string methods

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,4 +1,8 @@
-first = 'John'
-last = 'Smith'
-message = f'{first} [{last}] is a coder'
-print(message)
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.replace('Beginners', 'Absolute Beginners'))
+print('Python' in course)
+print(course.title())
