f29eec2f0f3c803f61fbd44071d56a8f28f7f416
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-20 12:09:00 +0300

    Getting Input

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,10 +1,2 @@
-price = 10
-price = 20
-rating =4.9
-name = 'Fred'
-is_published = False
-print(price)
-
-name = "John Smith"
-age = 20
-is_new = False
+name = input('What is your name? ')
+print('Hi ' + name)
