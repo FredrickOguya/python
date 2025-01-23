06f7abf03245d5d00defcdca6d6eceacf9def913
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-23 10:06:00 +0300

    while loops

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,10 +1,5 @@
-weight = int(input("Weight "))
-
-unit = input("(L)bs or (K)g: ")
-
-if unit.upper() == 'L':
-    converted_weight = weight * 0.45
-    print(f"you are {converted_weight} kilos")
-elif unit.upper() == 'K':
-    converted_weight = weight / 0.45
-    print(f"you are {converted_weight} pounds")
+i= 1
+while i<=5:
+    print('*' * i)
+    i+=1
+print("Done")
