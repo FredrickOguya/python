60056875d9abdce4c058d7792a4deee40fe8ba34
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-01-23 11:40:11 +0300

    For Loops

--- MOSH/app.py
+++ MOSH/app.py
@@ -1,28 +1,5 @@
-
-command = ""
-started = False
-while True:
-    command=input(">").lower()
-    if command == 'help':
-        print('''
-start - to start the car
-stop - to stop the car
-quit - to exit''')
-    elif command == 'start':
-        if started:
-            print('Car already started')
-        else:
-            started = True
-            print('Car started...Ready to go!')
-    elif command == 'stop':
-        if not started:
-            print('Car already stopped')
-        else:
-            started=True
-            print('Car stopped')
-    elif command == 'quit':
-        break
-    else:
-        print("I don't understand that")
-    command = input('')
-        
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(total)
